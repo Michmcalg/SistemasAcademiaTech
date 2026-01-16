fd4988b71b2ef945389be74cfe34c273e9ca9f0a
Mich <michmc.alg@gmail.com>
2026-01-15 23:22:21 -0500

    Primer commit de practica # with '#' will be ignored, and an empty message aborts the commit.

---
+++ Calculator.py
@@ -0,0 +1,19 @@
+print("*****MENÚ*****")
+print("1. Ingresar saldo")
+print("2. Mostrar saldo")
+print("3. Salir")
+opcion = int(input("Elige una opción: "))
+
+saldo = 0
+
+while opcion != 3:  
+    match opcion: 
+        case 1: 
+            saldo= float(input("Monto: "))
+        case 2: 
+            print(f"Su saldo es: ${saldo}")
+        case 3: 
+            print("¡Vuelva pronto!")
+        case _ :
+            print("Opción no válida")
+    opcion = int(input("Elige una opción: "))
---
+++ Validate.py
@@ -0,0 +1,12 @@
+while True:
+    user = input("Ingrese su usuario: ")
+
+    if len(user) > 12:
+        print("Su usuario no debe contener más de 12 caracteres.")
+    elif " " in user:
+        print("Su usuario no debe contener espacios.")
+    elif any(char.isdigit() for char in user): 
+        print("Su usuario no debe contener números.")
+    else:
+        print("Su usuario fue guardada con éxito.")
+        break
